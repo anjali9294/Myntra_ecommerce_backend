c4648ca673fad1691892e0b2702a581dd596944f
anjali9294 <anjaliparihar9294@gmail.com>
2023-03-03 10:45:22 +0530

    public path added

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use("/api", payment);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"));
 });
 
 // middileware for errors
